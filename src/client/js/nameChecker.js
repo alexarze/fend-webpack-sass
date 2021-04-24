dd016faecdf875fe6ef3bc2a7015cd012a7070e7
Alex Arze <alexarze@gmail.com>
2021-04-24 14:16:04 -0500

    Added fetch request to get weather in alert

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,4 +1,6 @@
-function checkForName(inputText) {
+const API_KEY = 'b15ae133a3c70b65373849d6da9a5b8a'
+
+async function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
     let names = [
         "Picard",
@@ -9,7 +11,14 @@ function checkForName(inputText) {
     ]
 
     if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid='+API_KEY)
+        const data = response ? await response.json() : null
+
+        if (data) {
+            alert("Welcome, Captain! The weather in " + data.name + " is " + data.weather[0].description)
+        } else {
+            alert("Welcome, Captain!")
+        }
     }
 }
 
